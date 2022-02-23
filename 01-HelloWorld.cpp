d66121c11bc92ddfde723b7dbb0435db043d0df2
xuhongyuan <xuhongyuan@momenta.ai>
2022-02-23 20:31:23 +0800

    hello world

---
+++ 01-HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <unistd.h>
+using namespace std;
+
+int main(){
+    cout << "hello" << endl;
+    // pause();
+    cout << "world" << endl;
+    return 0;
+}
+
+
---
+++ 02-注释.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+//1.单行注释
+//2.多行注释
+
+/*
+    main是一个程序的入口
+    每个程序都必须有这么一个函数
+    有且只有一个
+
+*/
+
+int main() {
+    //1行代码含义就是在屏幕中输出hello world
+    cout << "Hello Wolrd" << endl;
+
+}
