cb14e8dfd246fe042236798bca80c13903f207f6
xuhongyuan <xuhongyuan@momenta.ai>
2022-02-24 09:36:59 +0800

    2.5-转义字符

---
+++ C++基础入门/2 数据类型/2.5-转义字符.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    // 转义字符
+
+    // 换行符 \n
+
+    cout << "hello world\n";
+
+    // 反斜杠 \\
+
+    cout << "\\" << endl;
+
+    // 水平制表符 \t  作用可以整齐的输出数据
+    cout << "aaa\thelloworld" << endl;
+    cout << "aaaa\thelloworld" << endl;
+    cout << "aaaaa\thelloworld" << endl;
+
+    return 0;
+}
