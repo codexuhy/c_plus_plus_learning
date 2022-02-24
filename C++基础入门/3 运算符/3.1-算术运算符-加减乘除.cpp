35a2d5538e7c67375ad77814968bc0a7cb194a88
xuhongyuan <xuhongyuan@momenta.ai>
2022-02-24 10:21:23 +0800

    3.1-算术运算符-加减乘除

---
+++ C++基础入门/3 运算符/3.1-算术运算符-加减乘除.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //加减乘除
+    int a1 = 10;
+    int b1 = 3;
+
+    cout << a1 + b1 << endl;
+    cout << a1 - b1 <<endl;
+    cout << a1 * b1 <<endl;
+    cout << a1 / b1 << endl;
+
+    int a2 = 10;
+    int b2 = 20;
+    cout << a2/b2 << endl;
+
+    // int a3 = 10;
+    // int b3 = 0;
+    // cout << a3 / b3 << endl;  //错误！两个数相除，除数不能为0
+
+    double d1 = 0.5;
+    double d2 = 0.25;
+    cout << d1 / d2 << endl;
+    return 0;
+}
