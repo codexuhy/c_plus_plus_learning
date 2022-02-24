e1a517f4f97f53ea5a5605586bc3ceef8fcea1f6
xuhongyuan <xuhongyuan@momenta.ai>
2022-02-24 10:10:57 +0800

    2.8-数据的输入

---
+++ C++基础入门/2 数据类型/2.8-数据的输入.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // 1、整型
+    // int a = 0;
+    // cout << "请输入整型变量a：" << endl;
+    // cin >> a;
+    // cout << a << endl;
+
+    //2、浮点型
+    // double d = 0;
+    // cout << "请输入浮点型变量a：" << endl;
+    // cin >> d;
+    // cout << d << endl; 
+
+    //3、字符型
+    // char ch = 0;
+    // cout << "请输入字符类型变量：" << endl;
+    // cin >> ch;
+    // cout << ch << endl;
+
+    //4、字符串型
+    // string str ;
+    // cout << "请输入字符串类型变量：" << endl;
+    // cin >> str;
+    // cout << str << endl;
+
+    //5、布尔类型
+    bool flag = false;
+    cout << "请输入布尔类型flag：" << endl;
+    cin >> flag; // bool类型 只要是非0的值都代表真
+    cout << "布尔类型flag = " << flag << endl;
+    return 0;
+}
