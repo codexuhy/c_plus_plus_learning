b4e2888c351ac74fe3464c27f0167acd04dec48b
xuhongyuan <xuhongyuan@momenta.ai>
2022-04-10 12:40:18 +0800

    2.1-类模板语法(p167)

---
+++ C++提高编程/1.模板/2.类模板/2.1-类模板语法.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+// 类模板 
+template <class NameType, class AgeType>
+class Person
+{
+public:
+    Person(NameType name, AgeType age)
+    {
+        this->m_Name = name;
+        this->m_Age = age;
+    }
+
+    void showPerson(){
+        cout << "name: "<< m_Name << "age: "<< m_Age << endl;
+    }
+    NameType m_Name;
+    AgeType m_Age;
+};
+
+void test01(){
+    Person<string,int> p1("孙悟空",999);
+    p1.showPerson();
+}
+int main()
+{
+    test01();
+
+    return 0;
+}
