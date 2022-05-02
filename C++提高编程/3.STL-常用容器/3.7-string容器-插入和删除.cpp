2baf2653798f7e926b61a8b6e9f16f4ddf204102
xuhongyuan <xuhongyuan@momenta.ai>
2022-05-02 17:50:16 +0800

    3.7-string容器-插入和删除

---
+++ C++提高编程/3.STL-常用容器/3.7-string容器-插入和删除.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+//字符串插入和删除
+/*
+- string& insert(int pos,const char* s);      // 插入字符串
+- string& insert(int pos,const string& str);  // 插入字符串
+- string& insert(int pos,int n, char c);      // 在指定位置插入n个字符c
+- string& earese(int pos,int n = npos);       // 删除从Pos开始的n个字符
+*/
+
+void test01()
+{
+    string str = "hello";
+    str.insert(1, "111");
+    cout << str << endl;
+
+    str.erase(1, 3); //从1号位置开始3个字符
+    cout << str << endl;
+}
+
+int main()
+{
+    test01();
+
+    return 0;
+}
