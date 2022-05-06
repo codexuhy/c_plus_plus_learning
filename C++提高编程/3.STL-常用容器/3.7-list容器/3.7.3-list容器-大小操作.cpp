3625f5b65f672710baef681d028332beaa07cba7
xuhongyuan <xuhongyuan@momenta.ai>
2022-05-06 16:35:04 +0800

    3.7.3-list容器-大小操作

---
+++ C++提高编程/3.STL-常用容器/3.7-list容器/3.7.3-list容器-大小操作.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+#include <list>
+
+/*
+- empty();               //判断容器是否为空
+- size();                //返回容器中元素的个数
+- resize(int num);       //重新指定容器的长度为num,若容器变长，则以默认值填充新位置，
+                         //如果容器变短，则末尾超出容器长度的元素被删除。
+- resize(int num, elem); //重新指定容器的长度为num,若容器变长，则以emel值填充新位置
+                         //如果容器变短，则末尾超出容器长度的元素被删除
+*/
+
+void printList(const list<int> &L) //防止误操作修改容器里面的内容
+{
+    for (list<int>::const_iterator it = L.begin(); it != L.end(); it++)
+    {
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+
+void test01()
+{
+    list<int> L1;
+    L1.push_back(10);
+    L1.push_back(20);
+    L1.push_back(30);
+    L1.push_back(40);
+
+    printList(L1);
+
+    //判断容器是否为空
+    if (L1.empty())
+    {
+        cout << "L1不为空 " << endl;
+    }
+    else
+    {
+        cout << "L1不为空" << endl;
+        cout << "L1的元素个数为： " << L1.size() << endl;
+    }
+
+    //重新指定大小
+    L1.resize(10, 10000);
+    printList(L1);
+
+    L1.resize(2);
+    printList(L1);
+}
+
+int main()
+{
+    test01();
+
+    return 0;
+}
