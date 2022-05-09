221a2ecf71df65d216987eca7f652fc86c456242
xuhongyuan <xuhongyuan@momenta.ai>
2022-05-09 11:26:36 +0800

    5.1.2-常用遍历算法-transform

---
+++ C++提高编程/5.STL-常用算法/5.1.2-常用遍历算法-transform.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+#include<algorithm>
+// 常用遍历算法  transform
+class Transfrom
+{
+public:
+    int operator()(int val)
+    {
+        return val;
+    }
+};
+
+class MyPrint
+{
+public:
+    void operator()(int v)
+    {
+        cout << v << " ";
+    }
+};
+
+void test01()
+{
+    vector<int>v;
+    for(int i =0;i<10;i++)
+    {
+        v.push_back(i);
+    }
+
+    vector<int>vTarget;//目标容器
+    vTarget.resize(v.size());//目标容器需要提前开辟空间
+    
+    transform(v.begin(),v.end(),vTarget.begin(),Transfrom());
+
+    for_each(vTarget.begin(),vTarget.end(),MyPrint());
+    cout << endl;
+}
+
+
+int main()
+{
+    test01();
+    
+    return 0;
+}
