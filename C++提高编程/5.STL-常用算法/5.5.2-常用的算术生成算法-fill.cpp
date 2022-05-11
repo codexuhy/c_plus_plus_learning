93901e0a129d781669d4f62039d3e14396a53906
xuhongyuan <xuhongyuan@momenta.ai>
2022-05-11 19:42:55 +0800

    5.5.2-常用的算术生成算法-fill

---
+++ C++提高编程/5.STL-常用算法/5.5.2-常用的算术生成算法-fill.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <algorithm>
+
+//常用的算术生成算法 fill
+void myPrint(int val)
+{
+    cout << val << " ";
+}
+void test01()
+{
+    vector<int> v;
+    v.resize(10);
+
+    //后期重新填充
+    fill(v.begin(), v.end(), 100);
+
+    for_each(v.begin(), v.end(), myPrint);
+    cout << endl;
+}
+
+int main()
+{
+    test01();
+
+    return 0;
+}
