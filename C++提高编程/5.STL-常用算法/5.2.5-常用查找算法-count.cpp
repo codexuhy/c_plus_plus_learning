547f4c0bb859df6bd5ed0fa2fd88243d0a15db6e
xuhongyuan <xuhongyuan@momenta.ai>
2022-05-11 15:50:17 +0800

    5.2.5-常用查找算法-count

---
+++ C++提高编程/5.STL-常用算法/5.2.5-常用查找算法-count.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <algorithm>
+
+// 常用查找算法
+// 1、统计内置数据类型
+
+void test01()
+{
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(40);
+    v.push_back(30);
+    v.push_back(40);
+    v.push_back(20);
+
+    int num = count(v.begin(), v.end(), 40);
+    cout << "40的元素个数为：" << num << endl;
+}
+
+// 2、统计自定义数据类型
+class Person
+{
+public:
+    Person(string name, int age)
+    {
+        this->m_Name = name;
+        this->m_Age = age;
+    }
+    bool operator==(const Person &p)
+    {
+        if (this->m_Age == p.m_Age)
+        {
+            return true;
+        }
+    }
+
+    string m_Name;
+    int m_Age;
+};
+
+void test02()
+{
+    vector<Person> v;
+    Person p1("刘备", 35);
+    Person p2("关羽", 35);
+    Person p3("张飞", 35);
+    Person p4("赵云", 40);
+    Person p5("曹操", 40);
+
+    //将人员插入到容器中
+    v.push_back(p1);
+    v.push_back(p2);
+    v.push_back(p3);
+    v.push_back(p4);
+    v.push_back(p5);
+
+    Person p("诸葛亮", 35);
+    int num = count(v.begin(), v.end(), p);
+    cout << "和诸葛亮同岁数的人个数有：" << num << endl;
+}
+
+int main()
+{
+    test01();
+
+    test02();
+
+    return 0;
+}
